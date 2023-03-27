d34746b941fe695e831250b6a1356be08cef0d18
Carita Lindfors <caritalindf@gmail.com>
2023-03-27 14:22:48 +0300

    Dockerfile

--- backend/server.js
+++ backend/server.js
@@ -3,11 +3,7 @@ const cors = require('cors');
 
 const app = express();
 app.use(express.json());
-app.use(cors({
-  origin: [
-    'http://127.0.0.1:5500'
-  ]
-}));
+app.use(cors());
 
 const PORT = 5000;
 
